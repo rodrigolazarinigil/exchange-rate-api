0b63c0496d5a57355bb204e0de0e3ed3c6906b89
Rodrigo L. Gil <rodrigo.lazarini.gil@gmail.com>
2018-05-05 19:19:20 -0300

    Fix request format

--- request/fixer_io_requester.py
+++ request/fixer_io_requester.py
@@ -6,12 +6,12 @@ class FixerIoRequester:
 		self.latest_rate_url = \
 			"http://data.fixer.io/request_api/latest?access_key=c73bd725bbdbb2aac4a292fff231c619&base=EUR&symbols=USD"
 		self.historical_rate_by_range_url = \
-			"http://data.fixer.io/request_api/2018-01-01?access_key=c73bd725bbdbb2aac4a292fff231c619&base=EUR&symbols=USD"
+			"http://data.fixer.io/request_api/{date}?access_key=c73bd725bbdbb2aac4a292fff231c619&base=EUR&symbols=USD"
 
 	def get_latest_rate(self):
 		r = requests.get(url=self.latest_rate_url)
 		return r.json()
 
 	def get_historical_rate_by_range(self, date):
-		r = requests.get(url=self.historical_rate_by_range_url)
+		r = requests.get(url=self.historical_rate_by_range_url.format(date=date))
 		return r.json()
